597107d109c797976af8ffd329382acf6b437a54
Enzo Juarez <e.gustavojuarez@gmail.com>
2021-12-02 21:55:27 -0300

    Modificacion a Administracion.cpp

--- Administracion.cpp
+++ Administracion.cpp
@@ -7,25 +7,50 @@ struct profesionales{
         int id;
         int dni;
         char phone[25];
+};
+
+void registrar(FILE *p){
+        profesionales reg;
+
+        printf("\nCREAR CUENTA DE PROFESIONAL\n-----------------------------------\n");
+        printf("Apellido y nombre: ");
+        _flushall();
+        gets(reg.apenom);
+        printf("ID: ");
+        scanf("%d",&reg.id);
+        printf("DNI: ");
+        scanf("%d",&reg.dni);
+        printf("Numero de telefono: ");
+        _flushall();
+        gets(reg.phone);
+        fwrite(&reg,sizeof(profesionales),1,p);
+        
 }
 
-main(){
 
+
+main(){
+    FILE *p;
     int x;
 
     do{ 
-        printf("Módulo Administración\n==========================\n1.- Registrar Profesional\n2.- Registrar Usuario Recepcionista\n3.- Atenciones por Profesional\n5.- Ranking de Profesionales por Atenciones\n6.- Cerrar la aplicación.\nIngrese una opción: ");
+        printf("Modulo Administracion\n==========================\n1.- Registrar Profesional\n2.- Registrar Usuario Recepcionista\n3.- Atenciones por Profesional\n4.- Ranking de Profesionales por Atenciones\n5.- Cerrar la aplicacion.\nIngrese una opcion: ");
         scanf("%d",&x);
         switch(x){
-
-
-
-
-                case 6: default;
+                case 1: p=fopen("Profesionales.dat","ab");
+                        registrar(p);
+                        fclose(p);
+                        system("pause");
+                        system("cls");
                         break;
+
+
+
+                case 5: break;
+                        
         }  
 
-    }while(x!=6);
+    }while(x!=5);
 
 
 
